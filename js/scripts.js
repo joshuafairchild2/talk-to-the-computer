92233ece5a0270d394e346e4592e42d08661924b
Joshua Fairchild <joshua.fairchild429@gmail.com>
2017-02-06 08:56:36 -0800

    add a javascript function to each button that deletes the previous <li>s before the new <li>s are printed, as well as changes to the computer says text

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,43 @@
 $(function() {
   $("button#btn1").click(function() {
-    $("ul#you-said").prepend("<li>Hello computer!</li>")
-    $("ul#computer-said").prepend("<li>Beep boop beep beep boop.</li>")
+    $("ul#you-said").children("li").remove();
+    $("ul#computer-said").children("li").remove();
+    $("ul#you-said").prepend("<li>Hello computer!</li>");
+    $("ul#computer-said").prepend("<li>Hello human! Pleased to meet you.</li>");
   });
 
   $("button#btn2").click(function() {
-    $("ul#you-said").prepend("<li>Do you have any secrets?</li>")
-    $("ul#computer-said").prepend("<li>Beep boop beep beep boop.</li>")
+    $("ul#you-said").children("li").remove();
+    $("ul#computer-said").children("li").remove();
+    $("ul#you-said").prepend("<li>Do you have any secrets?</li>");
+    $("ul#computer-said").prepend("<li>Beep boop beep beep boop.</li>");
   });
 
   $("button#btn3").click(function() {
-    $("ul#you-said").prepend("<li>What is your name?</li>")
-    $("ul#computer-said").prepend("<li>Beep boop beep beep boop.</li>")
+    $("ul#you-said").children("li").remove();
+    $("ul#computer-said").children("li").remove();
+    $("ul#you-said").prepend("<li>What is your name?</li>");
+    $("ul#computer-said").prepend("<li>Seymour Files.</li>");
   });
 
   $("button#btn4").click(function() {
-    $("ul#you-said").prepend("<li>You look lovely today!</li>")
-    $("ul#computer-said").prepend("<li>Beep boop beep beep boop.</li>")
+    $("ul#you-said").children("li").remove();
+    $("ul#computer-said").children("li").remove();
+    $("ul#you-said").prepend("<li>You look lovely today!</li>");
+    $("ul#computer-said").prepend("<li>Does not compute.</li>");
   });
 
   $("button#btn5").click(function() {
-    $("ul#you-said").prepend("<li>Do you know any good computer jokes?</li>")
-    $("ul#computer-said").prepend("<li>Beep boop beep beep boop.</li>")
+    $("ul#you-said").children("li").remove();
+    $("ul#computer-said").children("li").remove();
+    $("ul#you-said").prepend("<li>Do you know any good computer jokes?</li>");
+    $("ul#computer-said").prepend("<li>What did the spider do on the computer? It made a website!</li>");
   });
 
   $("button#btn6").click(function() {
+    $("ul#you-said").children("li").remove();
+    $("ul#computer-said").children("li").remove();
     $("ul#you-said").prepend("<li>Goodbye computer!</li>")
-    $("ul#computer-said").prepend("<li>Beep boop beep beep boop.</li>")
+    $("ul#computer-said").prepend("<li>Goodbye human!</li>");
   });
 });
