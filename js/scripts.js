b0b8932cbf55f4f437638000c79cfc6156399fa0
Joshua Fairchild <joshua.fairchild429@gmail.com>
2017-03-02 15:37:40 -0800

    refactor the javascript logic using a foreach loop, move the output statements from scripts.js to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,11 @@
 $(function() {
-  $("button#btn1").click(function() {
-    $("ul#you-said").children("li").remove();
-    $("ul#computer-said").children("li").remove();
-    $("ul#you-said").prepend("<li>Hello computer!</li>");
-    $("ul#computer-said").prepend("<li>Hello human! Pleased to meet you.</li>");
-  });
-
-  $("button#btn2").click(function() {
-    $("ul#you-said").children("li").remove();
-    $("ul#computer-said").children("li").remove();
-    $("ul#you-said").prepend("<li>Do you have any secrets?</li>");
-    $("ul#computer-said").prepend("<li>Beep boop beep beep boop.</li>");
-  });
-
-  $("button#btn3").click(function() {
-    $("ul#you-said").children("li").remove();
-    $("ul#computer-said").children("li").remove();
-    $("ul#you-said").prepend("<li>What is your name?</li>");
-    $("ul#computer-said").prepend("<li>Seymour Files.</li>");
-  });
-
-  $("button#btn4").click(function() {
-    $("ul#you-said").children("li").remove();
-    $("ul#computer-said").children("li").remove();
-    $("ul#you-said").prepend("<li>You look lovely today!</li>");
-    $("ul#computer-said").prepend("<li>Does not compute.</li>");
-  });
-
-  $("button#btn5").click(function() {
-    $("ul#you-said").children("li").remove();
-    $("ul#computer-said").children("li").remove();
-    $("ul#you-said").prepend("<li>Do you know any good computer jokes?</li>");
-    $("ul#computer-said").prepend("<li>What did the spider do on the computer? It made a website!</li>");
-  });
-
-  $("button#btn6").click(function() {
-    $("ul#you-said").children("li").remove();
-    $("ul#computer-said").children("li").remove();
-    $("ul#you-said").prepend("<li>Goodbye computer!</li>")
-    $("ul#computer-said").prepend("<li>Goodbye human!</li>");
+  var buttonNumbers = ['1','2','3','4','5','6'];
+  buttonNumbers.forEach(function(buttonNumber) {
+    $("button#btn" + buttonNumber).click(function() {
+      $("#you-said").fadeIn();
+      $("#computer-said").fadeIn();
+      $("p").hide();
+      $(".phrase" + buttonNumber).show();
+    });
   });
 });
